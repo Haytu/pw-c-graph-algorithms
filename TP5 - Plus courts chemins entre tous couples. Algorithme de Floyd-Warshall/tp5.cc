efd27f0c6962c96cf858a1730dd5498dc91460bf
verniersolal <verniersolal@gmail.com>
2017-12-09 15:37:55 +0100

    exo5

--- TP5 - Plus courts chemins entre tous couples. Algorithme de Floyd-Warshall/tp5.cc
+++ TP5 - Plus courts chemins entre tous couples. Algorithme de Floyd-Warshall/tp5.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
-#include "villes.cc"
 
 using namespace std;
 
-const int n=5;
-const int inf=9999;                    //La valeur infinie.    
+// const int n=5; Exo 1 à 4
+const int inf=9999;  //La valeur infinie.
+const int n=6; // POur Fermeture transitive
 
 void floyd_warshall(int longueur[][n], int dist[][n], int chemin[n][n]){
 
@@ -88,6 +88,36 @@ void itineraire(int i, int j, int chemin[][n]){
     }
 }
 
+void fermeture_transitive(int arc[][n],int fermeture[][n]){
+
+  // Initialisation
+  for(int i = 0; i < n; i++){
+    for(int j=0; j < n; j++){
+      fermeture[i][j] ==  arc[i][j]; // Il y a forcément un chemin entre les sommets reliés de le matrice arc
+    }
+  }
+
+  for(int k=0; k < n; k++){
+    for(int i=0; i < n; i++){
+      for(int j=0; j < n; j++ ){
+        // Si fermeture[i][j] initialisé à 1, fermeture_transitive[i][j] reste 1.
+        // Si fermeture[i][j] initialisé à 0 MAIS qu'il existe (fermeture[i][k] == 1) ET(fermeture[k][j] == 1)  alors fermeture[i][j] devient 1
+          fermeture[i][j] = fermeture[i][j] || (fermeture[i][k] && fermeture[k][j]);
+        }
+    }
+  }
+
+  // Affichage
+  std::cout << "Matrice fermeture : " << '\n';
+  for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+          cout << fermeture[i][j] << " ";
+      }
+      cout << endl;
+  }
+  cout << endl;
+}
+
 int main(){
 
     int longueur[n][n]={{0,2,inf,4,inf}, //Les longueurs des arcs.
@@ -101,22 +131,19 @@ int main(){
     int chemin[n][n];                    //Le tableau de la premiere etape du chemin de i a j.
 
 //    floyd_warshall(longueur, dist, chemin);
-
+//
 //    int i, j;
 //    cout << "Entrer le départ : ";
 //    cin >> i;
-
+//
 //    cout << "Entrer la destination : ";
 //    cin >> j;
-
+//
 //    itineraire(i, j, chemin);
-    return 0;
-}
+//     return 0;
+// }
 
 
-/* Pour le calcul de la fermeture transitive:
-
-const int n=6;
 int arc[n][n]={{0,0,0,1,0,1},//La matrice d'adjacence du graphe oriente D.
                {1,0,1,1,0,0},
                {0,0,0,1,0,0},
@@ -124,7 +151,9 @@ int arc[n][n]={{0,0,0,1,0,1},//La matrice d'adjacence du graphe oriente D.
                {0,0,1,0,0,1},
                {0,0,1,0,0,0}};
 int fermeture[n][n];         // La matrice de la fermeture transitive de D.
+fermeture_transitive(arc,fermeture);
 
-*/
+}
+
 
 /* Exercice 1 : Montpellier Saint-Roch - Paris Gare de Lyon ; Paris Gare du Nord - Calais Frethun ; Calais Frethun - Calais Ville*/
